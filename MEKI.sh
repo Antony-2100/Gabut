e14c4d159ac65005ff3b1cdf05af8b8c8f0d6f8e
KamZ0 <74688013+KamZ0@users.noreply.github.com>
2020-11-30 03:18:29 +0700

    Update and rename MELI-YANI.sh to MEKI.sh

---
+++ MEKI.sh
@@ -0,0 +1,62 @@
+echo "INI TOOLS GABUT,JADI GAUSAH BAPER" 
+sleep 1
+echo "KALO BAPER MENDING KE POJOK WC AJA SANA" 
+sleep 2
+echo "OKEM MEK?" 
+sleep 3
+echo "===============================" 
+sleep 4
+echo "Kata Kata Akan segera Muncul" 
+sleep 5
+echo "==============================="
+
+echo "TOLONG DI BACA,HARGAI PEMBUAT"
+sleep 6
+echo "Untukmu Wanitaku" 
+sleep 7
+echo "Cintailah pria yang mencintaimu,bukan pria yang hanya kamu cintai"
+sleep 8
+echo "sebab pria yang kamu cintai akan pergi ke ujung dunia untukmu"
+sleep 9
+echo "tetapi pria yang kamu cintai hanya akan mencintaimu dengan persyaratan nya"
+sleep 10
+
+
+echo "×××××××××××××××" 
+sleep 11
+echo "NEXT QUATES" 
+sleep 12
+echo "×××××××××××××××" 
+sleep 13
+echo "Haii kamu" 
+sleep 14
+echo "Apa Kabar?" 
+sleep 15
+echo "Udah Lama ya Ga Kontekan" 
+sleep 16
+echo "Semoga Lu Selalu sehat Disana" 
+sleep 17
+echo "Oiya,Gimana kabar Keluarga" 
+sleep 18
+echo "Sehat Semuakan,Alhamdulillah"
+sleep 19
+echo "Gimana?Apa sekarang udah bahagia,bersama Yang baru?"
+sleep 20
+echo "Yang Gua harapkan,sekarang adalah Lu bahagia"
+sleep 21
+echo "Karena lu ninggalin gua demi dia"
+sleep 22
+echo "Ahh bnyk BACOT amjic:v" 
+sleep 23
+echo "Intinya Ditunggu Undangan nya"
+sleep 24
+echo "GUA BAYU PUTRA TAMA" 
+sleep 25
+echo "PAMIT" 
+sleep 26
+echo "ASSALAMUALAIKUM" 
+sleep 27
+
+echo "Meliyani && Bayu"
+sleep 18
+echo "Sheraton 18-2020"
