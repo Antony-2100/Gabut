8310a401abb4ed35ee55f308d0d92a4328d59a92
KamZ0 <74688013+KamZ0@users.noreply.github.com>
2020-11-30 05:29:48 +0700

    Update MEKI.sh

--- MEKI.sh
+++ MEKI.sh
@@ -1,3 +1,13 @@
+echo "--------------" 
+sleep 6
+echo "Author : Baiyu" 
+sleep 3
+echo "Tools  : Gabut :v" 
+echo "--------------" 
+sleep 9
+clear
+
+
 echo "INI TOOLS GABUT,JADI GAUSAH BAPER" 
 sleep 1
 echo "KALO BAPER MENDING KE POJOK WC AJA SANA" 
@@ -59,9 +69,53 @@ sleep 3
 echo "ASSALAMUALAIKUM" 
 sleep 4
 clear
-
-echo "Author : Bayu" 
+echo "Next" 
+sleep 3
+echo "````````````````" 
 sleep 5
-echo "Tools : Gabut"
+echo "Curahan Hati"
+sleep 5
+echo "````````````````" 
+sleel 5
+echo "Judul : Terlalu Sibuk"
+sleep 3
+echo "Maaf Ya kalo gua gak pernah ada waktu buat lu :("
+sleep 3
+echo "Dan maaf kalo gua gapernah bisa ngabarin lu sedikit pun :("
+sleep 3
+echo "Yang gua harapkan dari lu sih,cuma satu"
+sleep 3
+echo "Tolong jangan pernah berpaling dari gua"
+sleep 3
+echo "Tolong jangan pergi dari gua :("
+sleep 3
+echo "Gua disini Akan Jaga hati sekuat dan sebisa gua"
+sleep 3
+echo "Gua harap lu juga gitu" 
+sleep 3 
+echo "Memang menunggu itu enggak enak apa lagi,lu sampe nunggu berlari hari demi sebuah kabar dari gua" 
+sleep 3 
+echo "Ingatkan kata kata yg pernah lu omongin ke gua?" 
+sleep 3
+echo "Ya,Pasti inget kan" 
+sleep 3 
+echo "Yaitu : JAGA LAH HATI LU DI SAAT KITA JAUH,JAGA LAH PANDANGAN AGAR LU ENGGAK BERPALING DARI GUA,JANGAN PERNA LU MELONTARKAN KATA KATA PUTUS DI DALAM SUATU PERTENGKARAN" 
+sleep 3 
+echo "Tentu,Sudah gua pegang teguh kata kata dari lu"
+sleep 3 
+echo "Oiyaa,Jangan lupa makan yaaa!" 
+sleep 3 
+echo "Nanti penyakit MAAG lu kambuh lagi :("
+sleep 3 
+echo "Intinya Gua Sayang Lu :)" 
+sleep 3 
+echo "See You Next Time/Mount/Years"
+sleep 3
+echo "BANDAR LAMPUNG" 
+sleep 3 
+echo "30-11-2020"
+echo "Sekian Terima Rokok" 
+sleep 5
+echo "Wassallamuallaikum
 sleep 6
 date
