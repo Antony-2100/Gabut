34cc6d575bca226c1215f037992ced11f760fddc
KamZ0 <74688013+KamZ0@users.noreply.github.com>
2020-11-30 02:58:26 +0700

    Create MELI-YANI.php

---
+++ MELI-YANI.php
@@ -0,0 +1,25 @@
+echo "Silakan Instal Bahan Terlebih dahulu" 
+sleep 1
+echo "Sedang Proses Menginstall" 
+sleep 2
+apt update -y
+pkg instal git -y
+pkg install php -y
+pkg install nodejs -y
+pkg install wget -y
+echo "Selesai" 
+sleep 3
+echo "Ini Cuma Kegabutan gua aja"
+sleep 4
+echo "Untukmu Wanitaku" 
+sleep 5
+echo "Cintailah pria yang mencintaimu,bukan pria yang hanya kamu cintai"
+sleep 6
+echo "sebab pria yang kamu cintai akan pergi ke ujung dunia untukmu"
+sleep 7
+echo "tetapi pria yang kamu cintai hanya akan mencintaimu dengan persyaratan nya"
+sleep 8
+
+echo "Meliyani && Bayu"
+sleep 9
+echo "Sheraton 18-2020"
