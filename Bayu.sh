78fbf1886eab0cdb345baddcd5d242bb602450a8
KamZ0 <74688013+KamZ0@users.noreply.github.com>
2020-11-30 18:38:22 +0700

    Update Bayu.sh

--- Bayu.sh
+++ Bayu.sh
@@ -26,13 +26,13 @@ sleep 2
 echo "Untukmu Wanitaku" 
 sleep 2
 echo "Cintailah pria yang mencintaimu,bukan pria yang hanya kamu cintai"
-sleep 2
+sleep 1
 echo "sebab pria yang kamu cintai akan pergi ke ujung dunia untukmu"
-sleep 2
+sleep 1
 echo "tetapi pria yang kamu cintai hanya akan mencintaimu dengan persyaratan nya"
-sleep 6
+sleep 1
 echo "...." 
-sleep 18
+sleep 3
 clear
 
 
@@ -43,33 +43,33 @@ sleep 3
 echo "×××××××××××××××" 
 sleep 2
 echo "Haii kamu" 
-sleep 3
+sleep 1
 echo "Apa Kabar?" 
-sleep 3
+sleep 1
 echo "Udah Lama ya Ga Kontekan" 
-sleep 3
+sleep 1
 echo "Semoga Lu Selalu sehat Disana" 
-sleep 3
+sleep 1
 echo "Oiya,Gimana kabar Keluarga Lu disana" 
-sleep 3
+sleep 1
 echo "Sehat Semuakan,Alhamdulillah"
-sleep 3
+sleep 1
 echo "Gimana?Apa sekarang udah bahagia,bersama Yang baru?"
-sleep 3
+sleep 1
 echo "Yang Gua harapkan,sekarang adalah Lu bahagia"
-sleep 3
+sleep 1
 echo "Intinya teruslah berjuang"
-sleep 3
+sleep 1
 echo "Inget inget aje Tuh kata kata gua:v" 
-sleep 3
+sleep 1
 echo "Intinya Ditunggu Undangan nya"
-sleep 3
+sleep 1
 echo "GUA BAYU PUTRA TAMA" 
-sleep 5
+sleep 1
 echo "PAMIT" 
-sleep 3
+sleep 1
 echo "ASSALAMUALAIKUM" 
-sleep 4
+sleep 1
 clear
 echo "LANJUT" 
 sleep 3
@@ -82,52 +82,49 @@ sleep 5
 echo "Judul : Terlalu Sibuk"
 sleep 3
 echo "Maaf Ya kalo gua gak pernah ada waktu buat lu :("
-sleep 3
+sleep 1
 echo "Dan maaf kalo gua gapernah bisa ngabarin lu sedikit pun :("
-sleep 3
+sleep 1
 echo "Yang gua harapkan dari lu sih,cuma satu"
-sleep 3
+sleep 1
 echo "Tolong jangan pernah berpaling dari gua"
-sleep 3
+sleep 1
 echo "Tolong jangan pergi dari gua :("
-sleep 3
+sleep 1
 echo "Gua disini Akan Jaga hati sekuat dan sebisa gua"
-sleep 3
+sleep 1
 echo "Gua harap lu juga gitu" 
-sleep 3 
+sleep 1
 echo "Memang menunggu itu enggak enak apa lagi,lu sampe nunggu berlari hari demi sebuah kabar dari gua" 
-sleep 3 
+sleep 1
 echo "Ingatkan kata kata yg pernah lu omongin ke gua?" 
-sleep 3
+sleep 1
 echo "Ya,Pasti inget kan" 
-sleep 3 
+sleep 1
 echo "Yaitu : JAGA LAH HATI LU DI SAAT KITA JAUH,JAGA LAH PANDANGAN AGAR LU ENGGAK BERPALING DARI GUA,JANGAN PERNA LU MELONTARKAN KATA KATA PUTUS DI DALAM SUATU PERTENGKARAN" 
-sleep 3 
+sleep 1
 echo "Tentu,Sudah gua pegang teguh kata kata dari lu"
-sleep 3 
+sleep 1
 echo "Oiyaa,Jangan lupa makan yaaa!" 
-sleep 3 
+sleep 1 
 echo "Nanti penyakit MAAG lu kambuh lagi :("
-sleep 3 
+sleep 1
 echo "Intinya Gua Sayang Lu :)" 
-sleep 3 
+sleep 1
 echo "See You Next Time/Mount/Years"
-sleep 3
+sleep 1
 echo "BANDAR LAMPUNG" 
-sleep 3 
+sleep 1
 echo "30-11-2020"
 echo "Sekian Terima Rokok" 
-sleep 5
+sleep 1
 echo "Wassallamuallaikum"
-sleep 6
+sleep 1
 date
-sleep 8
+sleep 1
 
 
-
-
-
-
+clear
 echo " Besok lagi, kamu harus belajar mengert."
 sleep 3
 echo "Bahwa di atas perasaannya dia itu" 
@@ -136,3 +133,27 @@ echo "Ada Harga diri kamu"
 sleep 3
 echo "Yang enggak boleh kamu rendahin:)" 
 sleep 3
+clear
+echo "Untuk Yang Tersayang"
+sleep 1 
+echo "MeliYani" 
+sleep 1 
+echo "```````````" 
+sleep 3 
+echo "Jangan suka ngelawan abah" 
+sleep 1 
+echo "Jangan sering makan pedes" 
+sleep 
+echo "Jangan lupa mandi" 
+sleep 1 
+echo "Jangan Lupa Makan" 
+sleep 1 
+echo "Jangan lupa nyuci"
+sleep 1 
+echo "Jangan Sering ngeHALU" 
+sleep 1 
+echo "Dah itu Aja"
+sleep 1 
+echo "Intinya" 
+sleep 
+echo "Gua Bangga Bisa Milikin Lu:)" 
